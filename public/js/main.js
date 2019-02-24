587e1680008cf68d8b8edfaf174365b4305afb92
CloroxBleach6 <cloroxisgood.25@gmail.com>
2019-02-24 14:03:51 -0500

    added backgrounds

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
+import SpriteSheet from "./spritesheet.js";
+import {loadImage, loadLevel} from "./loaders.js";
+import {drawBackground} from "./functions.js";
 
 const canvas =  document.getElementById('screen')
 const context = canvas.getContext('2d')
 
- context.fillRect(0, 0, 50, 50)
-
 loadImage('/img/tiles.png')
 .then(image => {
-    context.drawImage(image, 0, 0);
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+
+    loadLevel('1-1').then(level => {
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, sprites)
+        });
+    });
 });
